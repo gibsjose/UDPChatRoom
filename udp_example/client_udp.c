78db58535444bb84dfe3ec91f6de0254173cbed7
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-02-04 11:28:25 -0500

    ...

---
+++ udp_example/client_udp.c
@@ -0,0 +1,97 @@
+//Example UDP client application
+
+#include <sys/socket.h> //Socket features
+#include <netinet/in.h> //Internet-specific features of sockets
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>   //timeval structure
+
+int main(int argc, char *argv[]) {
+
+    //Create a socket:
+    //socket() system call creates a socket, returning a socket descriptor
+    //  AF_INET specifies the address family for internet
+    //  SOCK_DGRAM says we want UDP as our transport layer
+    //  0 is a parameter used for some options for certain types of sockets, unused for INET sockets
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(sockfd < 0) {
+        printf("Could not open socket\n");
+        return -1;
+    }
+
+    //Create timeval struct
+    struct timeval to;
+    to.tv_sec = 5;
+    to.tv_usec = 0;
+
+    //Make socket only wait for 5 seconds w/ setsockopt
+    //  socket descriptor
+    //  socket level (internet sockets, local sockets, etc.)
+    //  option we want (SO_RCVTIMEO = Receive timeout)
+    //  timeout structure
+    //  size of structure
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &to, sizeof(to));
+
+    //Specify the address for the socket
+    //Create the socket address structure and populate it's fields
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;                            //Specify the family again (AF_INET = internet family)
+    serveraddr.sin_port = htons(9876);                          //Specify the port on which to send data (16-bit) (# < 1024 is off-limits)
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");        //Specify the IP address of the server with which to communicate
+                                                                //inet_addr() converts a string-address into the proper type
+
+    //No need for 'connect()' on a UDP connection
+        //Specify the socket descriptor, the server address structure, and the size of the socket address structure
+        //int e = connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(struct sockaddr_in));
+        // if(e < 0) {
+        //     printf("Could not connect to server\n");
+        //     return -1;
+        // }
+
+    //Can now send/receive data over the socket...
+
+    //Ask for some text
+    printf("Enter a line... ");
+
+    char s_line[1024];
+    char r_line[1024];
+    fgets(s_line, 1024, stdin);
+
+    //sendto: Like 'send()': Sends the data over the socket, except does not require
+    // a connection as in TCP:
+    //  socket descriptor
+    //  data (const void *)
+    //  size of the data
+    //  optional settings
+    //  Server we want to send to
+    //  Size of server structure
+
+    //With UDP, whatever is in the send buffer is explicitly sent in a packet
+    sendto(sockfd, s_line, strlen(s_line), 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
+
+    //recvfrom: Like 'recv()': Receive data over the socket:
+    //  socket descriptor
+    //  where to store data
+    //  MAX_BYTES
+    //  optional settings
+    //  store the address of what we received from in serveraddr
+    //  size of server address
+    //Returns the number of bytes received...
+    unsigned int len = sizeof(serveraddr);
+    int n = recvfrom(sockfd, r_line, 1024, 0, (struct sockaddr *)&serveraddr, &len);  //Receive is BLOCKING: Will wait for SOME data, but not necessarily until MAX_BYTES
+
+    if(n < 0) {
+        printf("Error receiving bytes from server...\n");
+        return -1;
+    } else if(n == 0) {
+        printf("No bytes were received from server...\n");
+    } else {
+        printf("Received %d bytes from server: %s\n", n, r_line);
+    }
+
+    //Close the socket
+    close(sockfd);
+
+    return 0;
+}
---
+++ udp_example/server_udp.c
@@ -0,0 +1,47 @@
+//Example UDP server application
+
+#include <sys/socket.h> //Socket features
+#include <netinet/in.h> //Internet-specific features of sockets
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+    //Open socket
+    //SOCK_DGRAM specifies a UDP socket (stands for Datagram)
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    //Create server/client address structures:
+    // Server needs to know both the address of itself and the client
+    struct sockaddr_in serveraddr;
+    struct sockaddr_in clientaddr;
+
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(9876);
+    serveraddr.sin_addr.s_addr = INADDR_ANY;    //Specifies the address on which the server will listen to for clients...
+                                                //INADDR_ANY specifies to listen on ANY address, but with a fixed port
+
+    //Bind: Similar to 'connect()': Associating the socket with the address, without contacting the address
+    //Bind, on the server side, tells all incoming data on the server address to go to the specified socket
+    bind(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
+
+    //No need to 'listen' on a UDP server: There are no connections
+
+    //Handle clients
+    while(1) {
+        int len = sizeof(clientaddr);
+
+        //Receive on the client socket
+        char r_line[1024];
+
+        unsigned int n = recvfrom(sockfd, r_line, 5000, 0, (struct sockaddr *)&clientaddr, &len);
+
+        //Display data received
+        printf("Received %d bytes from client: %s\n", n, r_line);
+
+        //Echo data back to client
+        sendto(sockfd, r_line, strlen(r_line), 0, (struct sockaddr *)&clientaddr, sizeof(clientaddr));
+
+        //No need to close UDP socket since there is only one for all clients
+    }
+}
